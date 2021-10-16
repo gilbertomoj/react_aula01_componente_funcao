6e7387dab64cbbe9c8aafc4666153e35c48d9e01
gilbertomoj <gibamedeirosgc@gmail.com>
2021-10-16 19:14:43 -0300

    Contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from "react";
 
-function App() {
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <center><h1>Contador </h1></center>
+      <center><h2>Você clicou {counter} vezes</h2></center>
+      <center>
+        <div className="buttons">
+        <button onClick={()=>{ setCounter(counter + 1)}}>Descremento</button>
+        <button onClick={()=>{ setCounter(counter - 1)}}>Incremento</button>
+        </div>
+      </center>
     </div>
   );
 }
-
-export default App;
